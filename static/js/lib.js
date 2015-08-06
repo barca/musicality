296d3c7fa3c0ec9821e7f9c7251bebde1ab5b01e
barca <tharden@wesleyan.edu>
2015-08-06 13:40:44 +0300

    chat app now works to specification

--- static/js/lib.js
+++ static/js/lib.js
@@ -6,31 +6,33 @@
 * @param {string} user this represents the user who sent the message
 * @param {string} message this represents the message sent to the room
 * @param {string} myName this represents the username of the local user
-* @param {Array.<string>} usersSeen represents all users who have sent messages within this rooms lifetime
+* @param {Array.<string>} usersSeen represents all users who have sent messages within this room's lifetime
 */
 function writeText(user, message, myName, usersSeen){
-    if(user == "myName"){
-        $(".text-output").append("<h3> "+ user + " : " + message + " </h3>");
+    if(user == myName){
+        $(".text-output").append("<h3 class='myName'> "+ user + " : " + message + " </h3>");
     }
     else {
-        if(usersSeen.indexOf(user) < 0){
-            usersSeen.push(user);
-            localStorage.setItem("user",usersSeen);
+        if(usersSeen.indexOf(user) == -1){
+            usersSeen.push(user)
+            localStorage.setItem("user",usersSeen.push());
         }
-       $(".text-output").append("<h3 class='user-" + usersSeen.indexOf(usersSeen) + 
+       $(".text-output").append("<h3 class='user-" + usersSeen.indexOf(user) + 
                                         "'> "+ user + " : " + message + " </h3>");
     }
 }
-
+/**
+* this is responsible for directing 
+* all keyboard typing into the chat bar
+*/
 $(document).bind("keydown",function(){
     $("#input").focus();
     $(document).unbind("keydown");
 });
-/** 
-* createIO takes two arguments, the name of the project on imrapid(musicality)
-* and the room name(blood, the name of the first song) 
+/**
+* pressing the enter key will trigger an ajax request
+* which sends the contents of the chatbar (defined by the ID input) to the room 
 */
-
 $(document).keypress(function(e){
     if (e.which == 13){
         	 $.ajax({
@@ -46,13 +48,18 @@ $(document).keypress(function(e){
     }
 });
 
-/**
-* The following portion of code listens for a message event after this, it inserts the 
-* recieved message into the DOM with writeText. It will then scroll down to the newest message  
+
+/** 
+* createIO takes two arguments, the name of the project on imrapid(musicality)
+* and the room name(blood, the name of the video) 
 */
 
 var room = createIO("musicality", "blood");
 
+/**
+* The following portion of code listens for a "message" event after this, it inserts the 
+* recieved message into the DOM with writeText. It will then scroll down to the newest message  
+*/
 room.on("message", function(data) {
 	writeText(data.user, data.message, localStorage.getItem("name") , JSON.parse(localStorage.getItem("users")));
 	var objDiv = document.getElementById("text");
@@ -62,7 +69,6 @@ room.on("message", function(data) {
 * the below segment of JS code is used for all the player logic, primarily taken from youtube's 
 * iframe API website.
 */
-
 var tag = document.createElement("player");
 tag.src = "https://www.youtube.com/iframe_api";
 var player;
@@ -70,12 +76,12 @@ function onYouTubePlayerAPIReady() {
     player = new YT.Player("player", {
       height: "100%",
       width: "100%",
-      videoId: 'KMe_5O0mYj0',
+      videoId: "KMe_5O0mYj0",
       playerVars:{
       "controls": 0,
       "showInfo": 0,
       "autoHide": 0,
-      "start" : parseInt(localStorage.getItem("secs")),
+      "start" : parseInt(localStorage.getItem("time")),
       "iv_load_policy": 3
       },
       events: {
@@ -83,20 +89,28 @@ function onYouTubePlayerAPIReady() {
       }
     });
     }
+
 function onPlayerReady(event) {
-    event.target.playVideo()
+    event.target.playVideo();
   }
+
+/**
+* this listens for load_event events, 
+* upon recieving such an event, it responds by giving 
+* the time elapsed in an ajax function
+*
+*/
 room.on("load_event", function(data) {
-    if(data.load_request_time === 0){
+    if(data.time === 0){
         $.ajax({
                 type: "POST",
                 url: "https://musicality.imrapid.io/load",
                 data: {
                     "video": "blood", 
-                    "load_request_time":  moment().diff(moment(localStorage.getItem("start")), "seconds")
+                    "time":  moment().diff(moment(localStorage.getItem("start")), "seconds")
                 },
                 });
-        alert("hey")
-    }
 
+    }
+    
 });
--- static/js/login.js
+++ static/js/login.js
@@ -1,13 +1,24 @@
 "use strict";
-function call(){
+/**
+* the navigateToMainContent function stores all relevent meta-data needed to run the app, and navigates to the main page
+* time stores the elapsed seconds since the start of the stream, its tell the youtube api where to begin the app
+* start, stores the timestamp (localized) when the stream began
+* users, an array that stores the names of the users who have previously spoken during this viewing session
+* users always starts as an empty array
+* name, indicates the name of the user on this machine, provided by the text input
+*/
+function navigateToMainContent(){
     var username = document.getElementById("username_input").value;
-    if(localStorage.getItem("load_request_time") == "0"){
+    if(localStorage.getItem("time") === "0"){
         localStorage.setItem("start", moment());
     }
     if (username.length > 3){
         localStorage.setItem("name", username);
         localStorage.setItem("users", JSON.stringify([]));
+        var time_since_start = moment().diff(moment(localStorage.getItem("start")), "seconds");
+        localStorage.setItem("time", t);
         window.location.assign("index.html");
+
         return true;        
     }
     else {
@@ -15,39 +26,60 @@ function call(){
         return false;
     }
 }
-document.getElementById("submit").onclick = function() {
-    call();
-    return true;
-};
-$(document).keypress(function(){
-    if (event.which == 13 || event.keyCode == 13){
-        event.preventDefault();
-        call();
-        return true;
+/** 
+* createIO takes two arguments, the name of the project on imrapid(musicality)
+* and the room name(blood, the name of the video) 
+*/
 
-    }
-});
 var room = createIO("musicality", "blood");
 
+/**
+* this listens for load_event events, 
+* upon recieving such an event from anyone already in session, it stores the time
+* it then calculates when the viewing event started, storing the localized time
+*
+*/
+room.on("load_event", function(data) {
+    if(data.time != "0"){
+        localStorage.setItem("time", data.time);
+        localStorage.setItem("start", moment().subtract(data.time, "seconds"));
+    }
+});
+
 $(document).ready(function(){
-    alert("sent")
-     $.ajax({
+    /**
+    * When the "listen Live!" button is clicked, navigateToMainContent is called, 
+    * and the value true is returned 
+    */
+    document.getElementById("submit").onclick = function() {
+        navigateToMainContent();
+        return true;
+    };
+    /**
+    * When the enter key is pressed, navigateToMainContent is called, 
+    * and the value true is returned, this allows the program to break out of the text input
+    */
+    $(document).keypress(function(){
+        if (event.which == 13 || event.keyCode == 13){
+            event.preventDefault();
+            navigateToMainContent();
+            return true;
+
+        }
+    });
+    /**
+    * When the site is first loaded, a request is sent to find anyone who is currently viewing 
+    * if an individual is watching, this ajax request will prompt them to provide 
+    * the seconds elapsed since the start of the video
+    */
+    $.ajax({
         type: "POST",
         url: "https://musicality.imrapid.io/load",
         data: {
             "video": "blood", 
-            "time": 120,
+            "time": 0,
         }
-        });
-     //localStorage.setItem("load_request_time", 0);
-
+    });
+    localStorage.setItem("time", 0);
 });
-room.on("load_event", function(data) {
-    //if(data.load_request_time != "0"){
-        localStorage.setItem("time", data.time);
-        localStorage.setItem("started", 0); //moment().subtract(data.load_request_time, "seconds"));
-        alert(localStorage.getItem("time"))
-    //}
-});
- 
 
